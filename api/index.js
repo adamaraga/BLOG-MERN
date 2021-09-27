d63faf8ac5aeb37a25189ef89be88deb2f444062
adamaraga <aragaadam@gmail.com>
2021-09-27 13:29:07 +0100

    add gitignore

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO_URL, {
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null,'images');
-  },
+  }, 
   filename: (req, file, cb) => {
     cb(null, req.body.name); 
   } 
